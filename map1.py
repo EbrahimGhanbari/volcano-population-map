682cc70a4ef5b606dd6fb9ad7b95ce2929237652
EbrahimGhanbari <ghanbari@ualberta.ca>
2020-09-03 22:01:14 -0600

    added external link to popup

--- map1.py
+++ map1.py
@@ -5,8 +5,13 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
 
-html = """<h4>Volcano information:</h4>
+
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
 Height: %s m
 """
 
@@ -16,8 +21,8 @@ fg = folium.FeatureGroup(name="My Map")
 
 # zip function help to go through the array at the same time
 
-for lt, ln, el in zip(lat, lon, elev):
-    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
     fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "green")))
  
 
