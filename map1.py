fd6b451b323f6edd272fff2ff346af72e8c3b52c
EbrahimGhanbari <ghanbari@ualberta.ca>
2020-09-03 21:46:30 -0600

    read from external file

--- map1.py
+++ map1.py
@@ -1,18 +1,17 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
 
 map =folium.Map(location=[38.58, -99.09], zoom_start=6, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.22, -99.1], popup="Hi I am a marker", icon=folium.Icon(color="green")))
+
+# zip function help to go through the array at the same time
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a marker", icon=folium.Icon(color="green")))
 map.add_child(fg)
 
 map.save("Map1.html")
-
-
-
-
-# tiles = "Mapbox Bright"
-
-# Please use this instead:
-
-# tiles = "Stamen Terrain"
