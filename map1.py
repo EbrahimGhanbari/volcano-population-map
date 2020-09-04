0bb12d6fe1d442c5dfda519e680593726c0661b1
EbrahimGhanbari <ghanbari@ualberta.ca>
2020-09-04 00:01:33 -0600

    cleaned up and add map layer

--- map1.py
+++ map1.py
@@ -7,7 +7,13 @@ lon = list(data["LON"])
 elev = list(data["ELEV"])
 name = list(data["NAME"])
 
-
+def colorProducer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
 
 html = """
 Volcano name:<br>
@@ -15,17 +21,26 @@ Volcano name:<br>
 Height: %s m
 """
 
+
 map =folium.Map(location=[38.58, -99.09], zoom_start=6, tiles = "Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanos")
 
 # zip function help to go through the array at the same time
-
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "green")))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe), radius=6, fill_color=colorProducer(el), color='grey', fill_opacity=0.7 ))
  
+fgp = folium.FeatureGroup(name="Population")
 
-map.add_child(fg)
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(), 
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else "red"}))
+
+# add layer to the map
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
 
 map.save("Map1.html")
